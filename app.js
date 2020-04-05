3d9ba20f5d4af2e564f679351616b613da650de0
jlam <jennylamwork@gmail.com>
2020-04-04 23:55:29 -0700

    updated app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const Intern = require('./employees/Intern.js')
 
 let employees = []
 
-const employQuestions = [
+const empQuestions = [
   'What is your name?:',
   'What is your ID?:',
   'What is your email?:',
@@ -23,3 +23,117 @@ const mngrQuestions = [
   'How many employees do you have?:',
   'How many interns do you have?:'
 ]
+
+async function getEmp() {
+  await prompt([
+    {
+      type: 'input',
+      name: 'empName',
+      message: empQuestions[0]
+    },
+    {
+      type: 'input',
+      name: 'empId',
+      message: empQuestions[1]
+    },
+    {
+      type: 'input',
+      name: 'empEmail',
+      message: empQuestions[2]
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: empQuestions[3]
+    }
+  ])
+  .then(response =>{
+    employees.push(new Engineer(
+      response.empName,
+      response.empId,
+      response.empEmail,
+      response.github
+    ))
+  })
+  .catch(e => console.error(e))
+}
+
+async function getInt() {
+  await prompt([
+    {
+      type: 'input',
+      name: 'intName',
+      message: empQuestions[0]
+    },
+    {
+      type: 'input',
+      name: 'intId',
+      message: empQuestions[1]
+    },
+    {
+      type: 'input',
+      name: 'intEmail',
+      message: empQuestions[2]
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: empQuestions[4]
+    }
+  ])
+    .then(response => {
+      employees.push(new Intern(
+        response.intName,
+        response.intId,
+        response.intEmail,
+        response.school
+      ))
+    })
+    .catch(e => console.error(e))
+}
+
+async function getManager() {
+  await prompt([
+    {
+      type: 'input',
+      name: 'mngrName',
+      message: mngrQuestions[0]
+    },
+    {
+      type: 'input',
+      name: 'mngrId',
+      message: mngrQuestions[1]
+    },
+    {
+      type: 'input',
+      name: 'mngrEmail',
+      message: mngrQuestions[2]
+    },
+    {
+      type: 'input',
+      name: 'office',
+      message: mngrQuestions[3]
+    },
+    {
+      type: 'input',
+      name: 'empNum',
+      message: mngrQuestions[4]
+    },
+    {
+      type: 'input',
+      name: 'intNum',
+      message: mngrQuestions[5]
+    }
+  ])
+    .then(response => {
+      employees.push(new Manager(
+        response.mngrName,
+        response.mngrId,
+        response.mngrmail,
+        response.office,
+        response.empNum,
+        response.intNum
+      ))
+    })
+    .catch(e => console.error(e))
+}
